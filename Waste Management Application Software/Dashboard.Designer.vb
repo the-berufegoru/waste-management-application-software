1ae104dcb72502722891a10a6e6dd3a6a751bcd3
D3m0n 533d <giftmoobi@gmail.com>
2022-09-09 22:48:16 +0200

    added dashboard and logo

---
+++ Waste Management Application Software/Dashboard.Designer.vb
@@ -0,0 +1,69 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Dashboard
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.picLogo = New System.Windows.Forms.PictureBox()
+        Me.Panel1.SuspendLayout()
+        CType(Me.picLogo, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.CornflowerBlue
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Panel1.Controls.Add(Me.picLogo)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(1370, 181)
+        Me.Panel1.TabIndex = 0
+        '
+        'picLogo
+        '
+        Me.picLogo.BackgroundImage = Global.Waste_Management_Application_Software.My.Resources.Resources.wastepic1
+        Me.picLogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.picLogo.Location = New System.Drawing.Point(10, 10)
+        Me.picLogo.Name = "picLogo"
+        Me.picLogo.Size = New System.Drawing.Size(264, 155)
+        Me.picLogo.TabIndex = 0
+        Me.picLogo.TabStop = False
+        '
+        'Dashboard
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.LightPink
+        Me.ClientSize = New System.Drawing.Size(1370, 609)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Dashboard"
+        Me.Text = "Dashboard"
+        Me.Panel1.ResumeLayout(False)
+        CType(Me.picLogo, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents picLogo As System.Windows.Forms.PictureBox
+
+End Class
